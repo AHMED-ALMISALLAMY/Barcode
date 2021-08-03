f6e00215bb72d52ff45216612ea5702c7e81b01c
AHMED-ALMISALLAMY <Almisallamy2@icloud.com>
2021-08-03 18:07:07 +0200

    first commit

---
+++ BL/Item.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Barcode.BL
+{
+    public class Item
+    {
+
+        #region variables
+
+        public int CategoryNumber { get; set; }
+
+        public decimal ItemPrice { get; set; }
+
+        public int ItemNumber { get; set; }
+
+        public string ItemName { get; set; }
+        #endregion
+
+
+
+        #region Methods
+
+        public string GetFormatedCategoryNumber(int number)
+        {
+            string NewNumber = number.ToString();
+
+            string returnedNumber = string.Empty;
+
+            for (int i = 0; i < 3 - NewNumber.Length; i++)
+            {
+                returnedNumber += "0";
+            }
+            return returnedNumber + NewNumber;
+        }
+
+
+        public string GetFormatedItemPrice(decimal number)
+        {
+            string NewNumber = number.ToString();
+
+            string returnedNumber = string.Empty;
+
+            for (int i = 0; i < 5 - NewNumber.Length;i++)
+            {
+                returnedNumber += "0";
+            }
+            return returnedNumber + NewNumber;
+        }
+
+
+        public string GetFormatedItemNumber(int number)
+        {
+            string NewNumber = number.ToString();
+
+            string returnedNumber = string.Empty;
+
+            for (int i = 0; i < 5 - NewNumber.Length;i++)
+            {
+                returnedNumber += "0";
+            }
+            return returnedNumber + NewNumber;
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Barcode.BL;
+using System.Collections.Generic;
+
+namespace Barcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // item category --> 3
+            // item price ----> 5
+            // item number ---> 5
+
+            string Barcodes = @"0010020000001-0020050000002-0030099900003-0040010000004";
+            string[] barcodesArray = Barcodes.Split('-');
+
+            Item OItem = new Item();
+
+            List<Item> LstItems = new List<Item>();
+            foreach (string barcode in barcodesArray)
+            {
+                OItem = new Item();
+                OItem.CategoryNumber = Convert.ToInt32(barcode.Substring(0, 3));
+                OItem.ItemPrice = Convert.ToInt32(barcode.Substring(3, 5));
+                OItem.ItemNumber = Convert.ToInt32(barcode.Substring(8, 5));
+                Console.WriteLine("Enter item name");
+                OItem.ItemName = Console.ReadLine();
+                LstItems.Add(OItem);
+            }
+
+            string NewBarCode = string.Empty;
+            Item myItem = new Item();
+            foreach (var item in LstItems)
+            {
+                NewBarCode += $"-{myItem.GetFormatedCategoryNumber(item.CategoryNumber)}{myItem.GetFormatedItemPrice(item.ItemPrice)}{myItem.GetFormatedItemNumber(item.ItemNumber)}";
+            }
+            Console.WriteLine(NewBarCode);
+        }
+    }
+}
